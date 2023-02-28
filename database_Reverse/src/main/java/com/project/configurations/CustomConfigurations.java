08d29d0b628e660eb8819610e897636251da62ed
AsifShaikh <shaikhasifraju@gmail.com>
2023-03-01 00:37:48 +0530

    email tested

---
+++ database_Reverse/src/main/java/com/project/configurations/CustomConfigurations.java
@@ -0,0 +1,16 @@
+package com.project.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.project.Service.EmailSenderUtilsService;
+
+@Configuration
+public class CustomConfigurations {
+
+	@Bean
+	public EmailSenderUtilsService getEmailService()
+	{
+		return new EmailSenderUtilsService();
+	}
+}
